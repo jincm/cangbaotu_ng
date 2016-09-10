f70967965bfaeb551de3ef7ebe6eda3f5b4a6c01
jincm <jincm@163.com>
2016-09-10 12:04:54 +0000

    add new modules of nearby message contact

---
+++ app/scripts/controllers/nearby.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cbtNgCssApp.controller:NearbyCtrl
+ * @description
+ * # NearbyCtrl
+ * Controller of the cbtNgCssApp
+ */
+angular.module('cbtNgCssApp')
+  .controller('NearbyCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
