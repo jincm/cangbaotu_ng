f57540b81bb69a5a654132887bbae70e97afeaba
jincm <jincm12@163.com>
2016-09-10 22:43:35 +0800

    增加底部导航中的内容，并可以切换

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -23,8 +23,15 @@ angular.module('cbtNgCssApp')
       //this.bigData = {};
       //this.bigData.breakfast = false;
       //COLLAPSE =====================
-      $scope.isCollapsed = false;
+      //$scope.isCollapsed = false;
 
+      //handle toggle navbar
+      $scope.currentIndex = 1; //default near is active
+      $scope.handle_toggle = function(click_index) {
+        $scope.currentIndex = click_index;
+      };
+
+      //test for user login and logout
       $scope.isLogin = function() {
         console.log('check if is login');
         if($window.localStorage.getItem('name') !== 'jincm') {
@@ -36,15 +43,11 @@ angular.module('cbtNgCssApp')
           return true;
         }
       };
-
       $scope.name = "jincm111";
       console.log('here');
-
       //$window.localStorage.setItem('name','jincm');
-
       console.log("main get is " + $window.localStorage.getItem('name'));
       console.log("auth is " + $scope.isUserAuth + " url is " + $location.path());
-
       $scope.logout = function () {
         console.log('logout success and clear');
         $window.localStorage.removeItem('name');
